5ef3136bfbfbc21ed79fe32d869dde2aa2b2ba9f
xremix <tonidhoffmann@gmail.com>
2019-05-02 20:13:35 +0200

    adjust port for azure

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 1337;
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(8089, function () {
-  console.log('Example app listening on port 8089!');
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 });
