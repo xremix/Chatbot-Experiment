f7bce36180fe9d6193fc1e9742412bc23150eb4b
xremix <tonidhoffmann@gmail.com>
2019-05-03 13:11:01 +0200

    implemented reply functionality

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var telegram = require('./telegram');
 var messageReciever = require('./message-reciever');
 var app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -10,19 +13,27 @@ var port = process.env.PORT || 1337;
 app.get('/', function (req, res) {
   res.send('Bot Service is up!');
 });
+
 app.get('/test', function (req, res) {
   telegram.getUpdates(process.env.TOKEN, function(){
     res.send('Done');
   });
 });
+
 app.get('/init', function (req, res) {
   res.send('Initialized Service with Telegram!');
 });
 
-app.get('/recievemessage', function (req, res) {
+app.post('/recievemessage', function (req, res) {
+  console.log(req.body);
   var userMessage = req.body.message.text;
+  var userId = req.body.message.from.id;
   var replyMessage = messageReciever.getReply(userMessage);
-  res.send(replyMessage);
+
+  telegram.sendMessage(process.env.TOKEN, userId, replyMessage, function(){
+    res.send(replyMessage);  
+  });
+
 });
 
 // app.get('/readMessages', function (req, res) {
@@ -32,6 +43,5 @@ app.get('/recievemessage', function (req, res) {
 // });
 
 app.listen(port, function () {
-  console.log('Example app listening on port ' + port);
-  console.log(process.env.TEST);
+  console.log('Service listening on http://localhost:' + port);
 });
--- telegram.js
+++ telegram.js
@@ -6,19 +6,33 @@ exports.setWebhook = function () {
   return Date();
 };
 exports.getUpdates = function (token, callback) {
-  request('https://api.telegram.org/bot'+token+'/getUpdates?offset=5', function (error, response, body) {
+  var reqUrl = 'https://api.telegram.org/bot'+token+'/getUpdates?offset=5';
+  console.log('sending request to ' + reqUrl);
+  request(reqUrl, function (error, response, body) {
     console.error('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:', body); // Print the HTML for the Google homepage.
-    callback();
+    callback && callback(response, body);
+  });
+};
+exports.sendMessage = function (token, id, text, callback) {
+  var reqUrl = 'https://api.telegram.org/bot'+token+'/sendMessage?chat_id=' + id + '&text=' + text;
+  console.log('sending request to ' + reqUrl);
+  request(reqUrl, function (error, response, body) {
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    callback && callback(response, body);
   });
 };
 
 exports.setWebhook = function (token, url, callback) {
-  request('https://api.telegram.org/bot'+token+'/setWebhook?url=' + url, function (error, response, body) {
+  var reqUrl = 'https://api.telegram.org/bot'+token+'/setWebhook?url=' + url;
+  console.log('sending request to ' + reqUrl);
+  request(reqUrl, function (error, response, body) {
     console.error('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:', body); // Print the HTML for the Google homepage.
-    callback();
+    callback && callback(response, body);
   });
 };
