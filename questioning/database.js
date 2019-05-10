3fbb4fb450c5c6cd9ae6ded052f0d401ddda8a97
xremix <tonidhoffmann@gmail.com>
2019-05-10 13:45:09 +0200

    Update database.js

--- questioning/database.js
+++ questioning/database.js
@@ -1,7 +1,7 @@
 exports.getUserPath = getUserPath;
 exports.appendUserPath = appendUserPath;
 exports.clearUserPath = clearUserPath;
-exports.version = '0.3.2';
+exports.version = '0.3.3';
 
 var jsondatabase = {
   user:{
